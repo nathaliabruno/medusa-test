87fdc454a40dc2a76398bd5933027b3d6160f3c5
Nathalia Bruno <nathi.bruno@gmail.com>
2022-11-22 19:15:47 -0300

    Add favicon and customize document

---
+++ pages/_document.tsx
@@ -0,0 +1,42 @@
+// import { MEDUSA_BACKEND_URL } from "@lib/config";
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <title>Nat Medusa Store</title>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href={`/apple-touch-icon.png`}
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href={`/favicon-32x32.png`}
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href={`/favicon-16x16.png`}
+          />
+          <link rel="manifest" href={`/site.webmanifest`} />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
