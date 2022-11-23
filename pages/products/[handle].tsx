ab40e7cefdcda4c451d585e0a782c02611dbfdc4
Nathalia Bruno <nathi.bruno@gmail.com>
2022-11-23 00:42:18 -0300

    add loading

--- pages/products/[handle].tsx
+++ pages/products/[handle].tsx
@@ -1,11 +1,21 @@
-import type { NextPage } from "next";
 import Head from "next/head";
+import type { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useProducts } from "medusa-react";
 
 const ProductPage: NextPage = () => {
+  const { query } = useRouter();
+
+  const { products, isLoading } = useProducts({
+    handle: query.handle,
+  });
+
+  const product = products[0];
+
   return (
     <>
       <Head>
-        <title>Nat Medusa Store</title>
+        <title>{product.title} | Nat Medusa Store</title>
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-centermin-w-screen"></main>
     </>
